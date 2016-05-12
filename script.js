bd2fc9d34402cd9ba484d1eaf125fc9d41ad4f85
rcadden <grcadden@gmail.com>
2016-05-12 10:17:37 -0500

    Update script.js
    Added ability to ask for chatter's name.

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ module.exports = new Script({
     start: {
         receive: (bot) => {
             return bot.say('So you want to learn about Ricky? Just say HELLO to get started.')
+                .then(() => 'askName');
+        }
+    },
+
+    askName: {
+        prompt: (bot) => bot.say('Before we get started, what\'s your name?'),
+        receive: (bot, message) => {
+            const name = message.text;
+            return bot.setProp('name', name)
+                .then(() => bot.say(`Great! I'll call you ${name}`))
                 .then(() => 'speak');
         }
     },
@@ -44,7 +54,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                    return bot.say(`Sorry, Ricky hasn't programmed me to respond to that yet.`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
